eca83fbfd53462137f6c3426760e09a8bce9472d
Natalia <natalia_gumowska@hotmail.com>
2025-03-08 14:48:50 +0100

    Update folder structure

--- scr/movie_rating.py
+++ scr/movie_rating.py
@@ -5,9 +5,9 @@ file_pattern = 'movies_rating.txt'
 
 rating = (
         p1
-        | beam.io.ReadFromText('raw/'+file_pattern, skip_header_lines=1)
+        | beam.io.ReadFromText('res/raw/'+file_pattern, skip_header_lines=1)
         | beam.Map(lambda x: x.split(','))
         | beam.Filter(lambda x: float(x[2]) > 4)
-        | beam.io.WriteToText('processed/'+file_pattern)
+        | beam.io.WriteToText('res/processed/'+file_pattern)
 )
 p1.run()
