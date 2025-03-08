e8fee5a6e9f9df73254e1d2c3a40b5c826f07233
Natalia <natalia_gumowska@hotmail.com>
2025-03-08 14:35:15 +0100

    Update movie_rating.py

--- scr/movie_rating.py
+++ scr/movie_rating.py
@@ -1,13 +1,13 @@
 import apache_beam as beam
 
 p1 = beam.Pipeline()
-file_pattern = 'raw/movies_rating.txt'
+file_pattern = 'movies_rating.txt'
 
 rating = (
         p1
-        | beam.io.ReadFromText(file_pattern, skip_header_lines=1)
+        | beam.io.ReadFromText('raw/'+file_pattern, skip_header_lines=1)
         | beam.Map(lambda x: x.split(','))
         | beam.Filter(lambda x: float(x[2]) > 4)
-        | beam.io.WriteToText('processed/movies_rating.txt')
+        | beam.io.WriteToText('processed/'+file_pattern)
 )
 p1.run()
